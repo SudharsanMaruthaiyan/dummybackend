993291c72d3a4e7b96f22c1674f7146a1cbdc5ec
SudharsanMaruthaiyan <sudharsan6078@gmail.com>
2024-10-10 19:42:32 +0530

    dfdfdgdfgdgffg

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json())
+
+// mongoose  connection 
+const mongoUrl = "mongodb+srv://sudharsan6078:123@cluster0.qvter.mongodb.net/";
+mongoose.connect(mongoUrl);
+
+const userSchema = new mongoose.Schema({
+    name:String,
+    age:Number
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.post("/", async (req,res)=>{
+    const userData = new User(req.body);
+    try{
+        const saveUser = await userData.save();
+        res.json({saveUser, message:"data stored"});
+    }
+    catch(err){
+        res.status(400).json({ message: err.message });
+    }
+})
+
+app.delete("/del", async (req,res)=>{
+    const {age} = new User(req.body);
+    try{
+        const delData = await User.deleteOne();
+        res.json(delData);
+    }
+    catch(err){
+        res.status(400).json({message: err.message})
+    }
+})
+
+
+
+app.get("/", (req,res)=>{
+    res.send("hello World")
+})
+
+
+const port = 3000;
+app.listen(port, ()=>{
+    console.log("Server ready")
+})
