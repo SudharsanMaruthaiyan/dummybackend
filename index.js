8a944d55189ccbce35d985635b7018f73e53ecbd
SudharsanMaruthaiyan <sudharsan6078@gmail.com>
2024-10-11 13:38:18 +0530

    asdfasdf

--- index.js
+++ index.js
@@ -37,20 +37,20 @@ app.post("/", async (req,res)=>{
     }
 })
 
-app.delete("/del", async (req,res)=>{
-    const {age} = new User(req.body);
-    try{
-        const delData = await User.deleteOne();
-        res.json(delData);
-    }
-    catch(err){
-        res.status(400).json({message: err.message})
-    }
-})
+// app.delete("/del", async (req,res)=>{
+//     const {age} = new User(req.body);
+//     try{
+//         const delData = await User.deleteOne();
+//         res.json(delData);
+//     }
+//     catch(err){
+//         res.status(400).json({message: err.message})
+//     }
+// })
 
 
 app.get("/", async (req,res)=>{
-
+    const userData = new User(req.params);
     try{
         const datata = await User.find();
         res.json(datata);
